edd1eea29081cdaf53d528c191b71031a200dc71
Marcy <marcela.klocker@southerninnovation.com>
2017-08-16 18:38:50 +1000

    made menu options c++ function exclusive

--- functions.cpp
+++ functions.cpp
@@ -8,40 +8,51 @@ int read_arg(int argc, char* argv[]){
   }
   else{
     for( int x = 1; x<9; x++){
-      if (strcmp("-s", argv[x])==0){
-        cout << "Sucess!" << endl;
-        data_structure();
+      if ("-s"== argv[x]){
+        if ("list"== argv[x+1]){
+
+        }
+
+        else if ("vector" == argv[x+1]){
+
+
+        }
+
+        else if ("set"==argv[x+1]){
+
+
+        }
+
+        else if ("custom list"==argv[x+1]){
+
+
+        }
+
+        else if ("custom tree"==argv[x+1]){
+
+
+        }
+
+        else{
+          cout<< "Sorry, the data structure you have chosen is not an option \n";
+        }
+
       }
 
-      if (strcmp("-d", argv[x])==0){
+      if ("-d"== argv[x]){
         cout << "Sucess!" << endl;
-        load_dictionary();
+
       }
 
-      if (strcmp("-t", argv[x])==0){
+      if ("-t"==argv[x]){
         cout << "Sucess!" << endl;
       }
 
-      if (strcmp("-o", argv[x])==0){
+      if ("-o"==argv[x]){
         cout << "Sucess!" << endl;
       }
 
     }
 
   }
-}
-// checks to see what data structure is required
-int data_structure(){
-
-
-}
-// loads dictionary
-int load_dictionary(){
-
-
-}
-
-
-int load_textfile(){
-
 }
